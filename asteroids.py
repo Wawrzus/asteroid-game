537d47722ad32c50ed298f7b77ba0450bb47bc8b
Szymon Wawrzyczek <szwawrzyczek@INZYNIERIA.LOCAL>
2024-12-13 11:35:29 +0100

    add asteroid colapse

--- asteroids.py
+++ asteroids.py
@@ -1,5 +1,7 @@
 import pygame
+import random
 from circleshape import CircleShape
+from constants import *
 
 class Asteroids(CircleShape):
     def __init__(self, x, y ,radius):
@@ -10,3 +12,16 @@ class Asteroids(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        angle = random.uniform(20, 50)
+        vec1 = self.velocity.rotate(angle)
+        vec2 = self.velocity.rotate(-angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid1 = Asteroids(self.position.x, self.position.y, new_radius)
+        asteroid2 = Asteroids(self.position.x, self.position.y, new_radius)
+        asteroid1.velocity = vec1 * 1.2
+        asteroid2.velocity = vec2 * 1.2
